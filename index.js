e82944889b892cf5df6a64b6013ec3b2c2724f38
NotYourSyn <60495598+NotYourSyn@users.noreply.github.com>
2020-03-09 23:58:14 -0400

    started JS1 assignment

--- index.js
+++ index.js
@@ -35,8 +35,8 @@ function addNumbers(num1, num2) {
  * the returned value should look like: 'Goodbye, Andy. Have a great day.'
  * 
 */
-function sayGoodbye(/* code here */) {
-  /* code here */
+function sayGoodbye(Andy) {
+  return 'Goodbye, ' +  Andy + '. ' + 'Have a great day.'
 }
 
 /**
